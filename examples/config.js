0de79cd8fefa99b4f1cd27fc2839af99983336b0
Luke Epp <lucasfepp@gmail.com>
2016-06-22 12:23:45 -0700

    Initial pages commit

--- examples/config.js
+++
@@ -1,5 +0,0 @@
-var pdjs_settings = {
-  subdomain: "webdemo",
-  token: "CkNpsqH9i6yTGus8VDzA",
-  refresh: 15,
-}
--- examples/configME.js
+++
@@ -1,8 +0,0 @@
-var pdjs_settings = {
-  subdomain: "pdt-circular",
-  token: "AbXSzhc8PzcJfujrzxMF", 
-  refresh: 60,
-  server: 'pagerduty.com',
-  protocol: 'https',  
-  logging: true,
-}
